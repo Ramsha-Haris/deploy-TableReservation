8f5ab15c175d9d6237ba24f7422ba33b5edf1e39
Ramsha-Haris <ramshamuzaffer@gmail.com>
2025-07-23 11:46:38 +0500

    Update axiosInstance.jsx
    updated basic url for deployment purpose

--- frontend/src/api/axiosInstance.jsx
+++ frontend/src/api/axiosInstance.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000', // your backend base URL
- //baseURL:'https://bodies-winter-democracy-nam.trycloudflare.com', 
+ // baseURL: 'http://localhost:3000', // your backend base URL
+ baseURL:'https://deploy-table-reservation-api.vercel.app/', 
  withCredentials: true,            // ✅ send cookies with requests
 });
 
