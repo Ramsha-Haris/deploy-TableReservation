743a2126764daae0fd32d5b20cc9000f04a42c2b
Ramsha-Haris <ramshamuzaffer@gmail.com>
2025-07-24 11:04:15 +0500

    Update axiosInstance.jsx

--- frontend/src/api/axiosInstance.jsx
+++ frontend/src/api/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
  // baseURL: 'http://localhost:3000', // your backend base URL
- baseURL:'https://deploy-table-reservation-api.vercel.app/', 
+ baseURL:'https://deploy-table-reservation-api.vercel.app', 
  withCredentials: true,            // ✅ send cookies with requests
 });
 
